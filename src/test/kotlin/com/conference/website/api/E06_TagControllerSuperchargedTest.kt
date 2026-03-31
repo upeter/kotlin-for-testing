c9b4855a265f6ec8d09240b41eab83b012d6de8d
upeter <peter.urs@gmail.com>
2026-03-31 09:36:31 +0200

    Add tests for listing tags and handling empty tag requests; refine Arrange-Act-Assert structure and assertions

--- src/test/kotlin/com/conference/website/api/E06_TagControllerSuperchargedTest.kt
+++ src/test/kotlin/com/conference/website/api/E06_TagControllerSuperchargedTest.kt
@@ -2,25 +2,28 @@ package com.conference.website.api
 
 import com.conference.website.data.createTagDto
 import com.conference.website.data.createTagsRequest
+import com.conference.website.dto.CreateTagsRequest
 import com.conference.website.service.TagService
-import com.conference.website.utils.defaultHeaders
-import com.conference.website.utils.jsonContent
 import com.conference.website.utils.objectMapper
 import com.ninjasquad.springmockk.MockkBean
 import io.kotest.matchers.collections.shouldContainInOrder
+import io.kotest.matchers.shouldBe
 import io.mockk.every
 import kom.conference.website.dto.TagDto
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest
 import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
+import org.springframework.http.ProblemDetail
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import tools.jackson.core.type.TypeReference
-import java.awt.PageAttributes
 import java.util.List
 
 @Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
@@ -56,6 +59,52 @@ class E06_TagControllerSuperchargedTest @Autowired constructor (
 
         //Assert
         createdTags.map { it.name } shouldContainInOrder tagRequest.names
+    }
+
+    @Test
+    fun `should list tags`() {
+        //Arrange
+        val initialTags = listOf("java", "testing")
+        val expectedTags = initialTags.mapIndexed { index, tag ->  createTagDto(index.toLong(), name = tag)  }
+        every { tagService.allTags } returns expectedTags
+
+        //Act
+        val response = mockMvc.perform(
+            MockMvcRequestBuilders.get("/api/tags")
+                .header("X-Correlation-Id", "1234567890")
+                .header("Authorization", "Bearer token")
+        )
+            .andExpect(status().isOk())
+            .andReturn()
+            .response
+            .contentAsString
+
+        val tags = objectMapper.readValue(response, object : TypeReference<List<TagDto>>() {})
+        //Assert
+        tags.map { it.name } shouldContainInOrder initialTags
+    }
+
+    @Test
+    fun `should return bad request when names is empty`() {
+        val request = CreateTagsRequest(emptyList())
+
+        val response = mockMvc.perform(
+            post("/api/tags")
+                .header("X-Correlation-Id", "1234567890")
+                .header("Authorization", "Bearer token")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+        )
+            .andExpect(status().isBadRequest())
+            .andReturn()
+            .response
+            .contentAsString
+
+        val errorResponse = objectMapper.readValue(response, ProblemDetail::class.java)
+        errorResponse.apply {
+            title shouldBe "Validation failure"
+            detail shouldBe "Request validation failed"
+        }
     }
 
 }
